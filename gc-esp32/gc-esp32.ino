130ff818b86f59ce1d786a834926270b0ed605e2
Andreas Kleber <andreas@drosselweg7a.de>
2022-07-21 07:52:46 +0200

    my first version

---
+++ gc-esp32/gc-esp32.ino
@@ -0,0 +1,209 @@
+ /*
+   Geiger.ino
+
+   This code interacts with the Alibaba RadiationD-v1.1 (CAJOE) Geiger counter board
+   and reports on Display readings in CPM (Counts Per Minute).
+   Also works with Alibaba "IOT Geiger Counter" Model GC-ESP32 CAJOE.
+   Connect the output of the Geiger counter to pin inputPin.
+
+   Install ThingPulse SSD1306 Library
+   Install the board support as described here (for esp_task_wdt.h):
+   https://docs.espressif.com/projects/arduino-esp32/en/latest/installing.html#installing-using-boards-manager
+
+   Author Andreas Kleber
+   Based on https://github.com/SensorsIot/Geiger-Counter-RadiationD-v1.1-CAJOE-
+   By Hans Carlos Hofmann, Andreas Spiess
+   Based on initial work of Mark A. Heckler (@MkHeck, mark.heckler@gmail.com)
+   License: MIT License
+   Please use freely with attribution. Thank you!
+*/
+
+#define Version "V1.0.0"
+#define CONS
+#define WIFI
+#ifdef CONS
+#define PRINT_DEBUG_MESSAGES
+#endif
+
+#include "credentials.h"
+// #define mySSID ""
+// #define myPASSWORD ""
+
+#ifdef WIFI
+#include <WiFi.h>
+#endif
+
+#include <SSD1306.h>
+SSD1306  display(0x3c, 5, 4);
+
+#include <esp_task_wdt.h>
+
+#define WIFI_TIMEOUT_DEF 30
+#define PERIOD_LOG 15                //Logging period 
+#define PERIOD_THINKSPEAK 3600        // in seconds, >60
+#define WDT_TIMEOUT 10
+
+
+const int inputPin = 26;
+
+int counts = 0;  // Tube events
+int counts2 = 0;
+int cpm = 0;                                             // CPM
+unsigned long lastCountTime;                            // Time measurement
+unsigned long lastEntryThingspeak;
+unsigned long startCountTime;                            // Time measurement
+unsigned long startEntryThingspeak;
+
+
+void IRAM_ATTR ISR_impulse() { // Captures count of events from Geiger counter board
+      counts++;
+      counts2++;
+}
+
+
+void displayInit() {
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_16);
+  display.clear();
+}
+
+void displayString(String dispString, int x, int y) {
+  display.setColor(WHITE);
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.drawString(x, y, dispString);
+  display.setFont(ArialMT_Plain_16);
+  display.display();
+}
+
+
+/****reset***/
+void software_Reset() // Restarts program from beginning but does not reset the peripherals and registers
+{
+  printlnSerial("resetting by software");
+  displayString("Myreset", 64, 15);
+  delay(1000);
+  esp_restart();
+}
+
+void printStack()
+{
+#ifdef CONS
+  char *SpStart = NULL;
+  char *StackPtrAtStart = (char *)&SpStart;
+  UBaseType_t watermarkStart = uxTaskGetStackHighWaterMark(NULL);
+  char *StackPtrEnd = StackPtrAtStart - watermarkStart;
+  Serial.printf("=== Stack info === ");
+  Serial.printf("Free Stack is:  %d \r\n", (uint32_t)StackPtrAtStart - (uint32_t)StackPtrEnd);
+#endif
+}
+
+void printlnSerial(String msg)
+{
+#ifdef CONS
+  Serial.println(msg);
+#endif 
+}
+void printSerial(String msg)
+{
+#ifdef CONS
+  Serial.print(msg);
+#endif 
+}
+
+void setup() {
+  esp_task_wdt_init(WDT_TIMEOUT, true); //enable panic so ESP32 restarts
+  esp_task_wdt_add(NULL); //add current thread to WDT watch
+  
+#ifdef CONS
+  Serial.begin(115200);
+#endif
+
+  printSerial("This is ") ;
+  printlnSerial(Version) ;
+
+  displayInit();
+  displayString("Startup", 64, 0);
+  displayString(Version, 64, 30);
+  printStack();
+#ifdef WIFI
+  printlnSerial("Connecting to Wi-Fi");
+
+  WiFi.begin(mySSID, myPASSWORD);
+
+  int wifi_loops = 0;
+  int wifi_timeout = WIFI_TIMEOUT_DEF;
+  while (WiFi.status() != WL_CONNECTED) {
+    wifi_loops++;
+    printSerial(".");
+    delay(500);
+    if (wifi_loops > wifi_timeout) software_Reset();
+  }
+  printlnSerial("");
+  printlnSerial("Wi-Fi Connected");
+#endif
+  display.clear();
+  displayString("Measuring", 64, 15);
+  pinMode(inputPin, INPUT);                            // Set pin for capturing Tube events
+  printlnSerial("Defined Input Pin");
+  attachInterrupt(digitalPinToInterrupt(inputPin), ISR_impulse, FALLING);     // Define interrupt on falling edge
+  Serial.println("Irq installed");
+
+  startEntryThingspeak = lastEntryThingspeak = millis();
+  startCountTime = lastCountTime = millis();
+  printlnSerial("Initialized");
+}
+
+int active = 0 ;
+
+void loop() {
+  esp_task_wdt_reset();
+#ifdef WIFI
+  if (WiFi.status() != WL_CONNECTED) software_Reset();
+#endif
+
+  if (millis() - lastCountTime > (PERIOD_LOG * 1000)) {
+    printSerial("Counts: ");
+    printlnSerial(String(counts));
+
+    cpm = (60000 * counts) / (millis() - startCountTime) ;
+    counts = 0 ;
+    startCountTime = millis() ;
+    lastCountTime += PERIOD_LOG * 1000;
+    display.clear();
+    displayString("Radioactivity", 64, 0);
+    displayString(String(cpm), 64, 30);
+    if (cpm >= 200) {
+      if (active) {
+        active = 0 ;
+        display.clear();
+        displayString("¡¡ALARM!!", 64, 0);
+        displayString(String(cpm), 64, 30);
+      } ;
+    }
+    else if (cpm < 100)
+    {
+      active = 1 ;
+    } ;
+    printSerial("cpm: ");
+    printlnSerial(String(cpm));
+
+    printStack();
+  }
+
+  if (millis() - lastEntryThingspeak > (PERIOD_THINKSPEAK * 1000)) {
+    printSerial("Counts2: ");
+    printlnSerial(String(counts2));
+
+    int averageCPH = (int)(((float)3600000 * (float)counts2) / (float)(millis() - startEntryThingspeak));
+
+    printSerial("Average cph: ");
+    printlnSerial(String(averageCPH));
+
+    //postThingspeak(averageCPH);
+    lastEntryThingspeak += PERIOD_THINKSPEAK * 1000;
+    startEntryThingspeak = millis();
+    counts2=0;
+  } ;
+  delay(50);
+}
